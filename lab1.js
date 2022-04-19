0977262056830647cb611ee9af4c6eaf69252267
Matthew Araujo <90223014+MatthewAraujo@users.noreply.github.com>
2022-04-19 10:02:22 -0300

    Add files via upload

---
+++ lab1.js
@@ -0,0 +1,36 @@
+/*
+1.Crie um script JS que leia o nome de uma pessoa e mostre uma mensagem de Boas Vindas de acordo com o nome digitado.
+
+var nome = prompt('Digite seu nome: ')
+alert(`Seja bem vindo ${nome}`)*/
+
+/*
+2.Ler dois valores a partir do prompt, calcular e imprimir na tela:
+a) A soma destes valores;
+b)O produto deles;
+c)O quociente entre eles;
+d)A diferença entre eles.
+
+var n1 = +(prompt('Digite um numero: '))
+var n2 = +(prompt('Digite outro numero: '))
+var soma = n1 + n2
+var prod = n1 * n2
+var quoc = n1 / n2
+var dif = n1 - n2
+document.write(`A) ${soma}<br>B) ${prod}<br>C) ${quoc}<br>D) ${dif}`)
+*/
+/*
+3.Elabore um programa em JS que calcule a média aritmética de duas notas e exiba o resultado na tela.
+
+var av1 = parseFloat(prompt('Digite sua av1: '))
+var av2 = parseFloat(prompt('Digite sua av2: '))
+var media = (av1 + av2) / 2
+
+document.write("Sua media é: ", media)
+*/
+/*
+4.Desenvolva um programa em JS em que a variável 1 recebe o valor 5, a variável 2 recebe 3 e a variável 3 recebe o resultado da subtração de var 1 e var 2 e mostre na tela a frase " Resultado da subtração é: concatenado com o valor do resultado.*/
+var num1 = 5
+var num2 = 3
+var num3 = num1 - num2
+document.write("Resultado da subtração é: ", num3)
